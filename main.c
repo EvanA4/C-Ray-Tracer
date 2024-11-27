55edf132cbdc2576868002506a18bea076dd0766
EvanA4 <evanabbott04@gmail.com>
2024-11-27 13:38:55 -0500

    added basic tga file writing

--- main.c
+++ main.c
@@ -4,22 +4,25 @@
 #include <pthread.h>
 #include "args.h"
 #include "tpool.h"
+#include "tga.h"
 
 
 int main(int argc, char **argv) {
+    image_writer();
     struct KerrArgs *args = parse_args(argc, argv);
     if (!args) return 1;
     
     // create thread pool
     struct TPool *mypool = tpool_init(args);
 
-    // read input from user
     while (1) {
+        // read input from user
         char *input = NULL;
         size_t allocd = 0;
         size_t numRead = getline(&input, &allocd, stdin);
         input[numRead - 1] = '\0';
 
+        // send input to worker to print
         if (!strcmp(input, "exit")) {
             free(input);
             tpool_close(mypool);
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 all:
-	gcc -Wall -Wextra -o kerr main.c args.c tpool.c -lpthread
+	gcc -Wall -Wextra -o kerr main.c args.c tpool.c tga.c -lpthread
 clean:
 	rm donut
